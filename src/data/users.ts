3a332af6ae22ff3f42b471b09bb678c943cce8fd
Tenderly__ <olevnao31@gmail.com>
2026-03-26 18:42:56 +0300

    fix duplicate ids in users data

--- src/data/users.ts
+++ src/data/users.ts
@@ -4,7 +4,7 @@ export const users: User[] = [
   { id: 1, name: "Pavel", age: 19, city: "Seoul", role: "admin" },
   { id: 2, name: "Chris", age: 27, city: "Raccoon sity", role: "user" },
   { id: 3, name: "Alfred", age: 23, city: "Berlin", role: "user" },
-  { id: 1, name: "Chon", age: 19, city: "Seoul", role: "moderator" },
-  { id: 1, name: "Kim", age: 20, city: "Busan", role: "user" },
-  { id: 1, name: "Luna", age: 26, city: "Paris", role: "user" },
+  { id: 4, name: "Chon", age: 19, city: "Seoul", role: "moderator" },
+  { id: 5, name: "Kim", age: 20, city: "Busan", role: "user" },
+  { id: 6, name: "Luna", age: 26, city: "Paris", role: "user" },
 ];
