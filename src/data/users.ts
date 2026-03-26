a6123e1f2f9b7a560e7c64f0d1b8cc4fdc44920a
Tenderly__ <olevnao31@gmail.com>
2026-03-26 18:40:21 +0300

    feat: add users data

---
+++ src/data/users.ts
@@ -0,0 +1,10 @@
+import type { User } from "../types/user";
+
+export const users: User[] = [
+  { id: 1, name: "Pavel", age: 19, city: "Seoul", role: "admin" },
+  { id: 2, name: "Chris", age: 27, city: "Raccoon sity", role: "user" },
+  { id: 3, name: "Alfred", age: 23, city: "Berlin", role: "user" },
+  { id: 1, name: "Chon", age: 19, city: "Seoul", role: "moderator" },
+  { id: 1, name: "Kim", age: 20, city: "Busan", role: "user" },
+  { id: 1, name: "Luna", age: 26, city: "Paris", role: "user" },
+];
